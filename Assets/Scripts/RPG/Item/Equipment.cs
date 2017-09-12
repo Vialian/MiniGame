8006687e4876689fbeca85c59f9ee03b0039cbd1
Vialian <dotasoren@gmail.com>
2017-09-12 14:57:35 +0200

    armor

--- Assets/Scripts/RPG/Item/Equipment.cs
+++ Assets/Scripts/RPG/Item/Equipment.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* An Item that can be equipped. */
+
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
-public class Equipment : Item {
+public class Equipment : Item
+{
 
-    public EquipmentSlot equipSlot;
+    public EquipmentSlot equipSlot;	// Slot to store equipment in
+    public SkinnedMeshRenderer mesh;
+    public EquipmentMeshRegion[] coveredMeshRegions;
 
-    public int armorModifer;
-    public int damageModifier;
+    public int armorModifier;       // Increase/decrease in armor
+    public int damageModifier;      // Increase/decrease in damage
 
-
+    // When pressed in inventory
     public override void Use()
     {
         base.Use();
-        //Equip item
-        EquipmentManager.instance.Equip(this);
-        //remove from inventory
-        RemoveFromInventory();
+        EquipmentManager.instance.Equip(this);  // Equip it
+        RemoveFromInventory();                  // Remove it from inventory
     }
+
 }
 
-public enum EquipmentSlot { Head,Chest,Legs,Weapon,Shield,Feet}
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet }
+public enum EquipmentMeshRegion { Legs, Arms, Torso }; // Corresponds to body blendshapes.
